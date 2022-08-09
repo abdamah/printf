348663af2d84ab6c14cd5454dcd1457807a8cd75
Abdallah <abdamoh1@gmail.com>
2022-08-09 12:54:30 -0700

    Update main.c adding test line for rev string.

--- test/main.c
+++ test/main.c
@@ -41,7 +41,7 @@ int main(void)
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-	_printf("Unknown:[%r]\n");
+	_printf("Unknown:[%r]\n", "ALX School");
 	/*printf("Unknown:[%r]\n");*/
 		return (0);
 }
