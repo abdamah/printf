63e55f2a220d78d8965aa3ae0432a0410fd2356b
zyzer01 <davidicfola@gmail.com>
2022-08-09 17:00:45 +0100

    Updated documentation on _printf.c and print_binary.c file

--- print_binary.c
+++ print_binary.c
@@ -2,7 +2,7 @@
 
 /**
  * print_binary - function that prints the binary representation of a number
- * @n: number to be printed in binary
+ * @n: the number to be printed in binary
  * @printed: hold the number of characters printed
  */
 void print_binary(unsigned int n, unsigned int *printed)
