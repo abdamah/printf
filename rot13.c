396908a42648a8dc84fb5adb7d58769fe018bb36
Abdallah <abdamoh1@gmail.com>
2022-08-10 06:31:56 -0700

    Fix: rot13 function.

--- rot13.c
+++ rot13.c
@@ -2,47 +2,31 @@
 /**
  * rot13 - encode srting into rot13
  * @arg: string to encode
- * Return: (point)
+ * Return: int
  */
 int rot13(va_list arg)
-{	int i = 0, j, count = 0, flag = 0;
-	char *point = va_arg(arg, char *);
-	char lett[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G',
-'H', 'I', 'J', 'K', 'L', 'M', 'N',
-'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-'V', 'W', 'X', 'Y', 'Z', 'a', 'b',
-'c', 'd', 'e', 'f', 'g', 'h', 'i',
-'j', 'k', 'l', 'm', 'n', 'o', 'p',
-'q', 'r', 's', 't', 'u', 'v', 'w',
-'x', 'y', 'z'};
-	char rot[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-'U', 'V', 'W', 'X', 'Y', 'Z', 'A',
-'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-'J', 'K', 'L', 'M', 'n', 'o', 'p',
-'q', 'r', 's', 't', 'u', 'v', 'w',
-'x', 'y', 'z', 'a', 'b', 'c', 'd',
-'e', 'f', 'g', 'h', 'i', 'j', 'k',
-'l', 'm'};
-	if (!point)
+{
+	register short i, j = 0;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *s = va_arg(arg, char *);
+
+	if (!s[j])
 	{
-		point = "(null)";
+		return (-1);
 	}
-	while (point[i])
+
+	for (j = 0; s[j]; j++)
 	{
-		for (j = 0; j < 52; j++)
+		if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
+			_putchar(s[j]);
+		else
 		{
-			if (point[i] == lett[j])
-			{
-				_putchar(rot[j]);
-				count++;
-				flag = 1;
-				break;
-			}
+			for (i = 0; i <= 52; i++)
+				if (s[j] == rot13[i])
+					_putchar(ROT13[i]);
 		}
-		if (!flag)
-			_putchar(point[i]);
-		flag = 0;
-		i++;
 	}
-	return (count);
+	return (j);
 }
+
